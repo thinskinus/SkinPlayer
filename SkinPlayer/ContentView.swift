fa814b592aff0eee115673c3f52a6a72b68de109
Elena Demidova <thinskinus@gmail.com>
2024-10-11 15:24:17 +0300

    First commit

--- SkinPlayer/ContentView.swift
+++ SkinPlayer/ContentView.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+
+    @ObservedObject var vm: ViewModel = .init()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Play: Skin - I'll try")
         }
         .padding()
+        .onAppear {
+            do {
+                try vm.playSong()
+            } catch {
+                print("🔴 error \(error)")
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+class ViewModel: ObservableObject {
+    var audioPlayer: AVAudioPlayer?// = AVAudioPlayer()
+
+    func playSong() throws {
+        // describe the bundle
+        let bundle = Bundle.main
+        // access the song
+        guard let song1 = bundle.path(forResource: "Skin - I'll Try", ofType: "mp3") else { return }
+        // check this sound with audioPlayer
+//        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: song1))
+//            print("sound found")
+//        } catch {
+//            print("sound not found")
+//        }
+        
+        audioPlayer?.play()
+    }
 }
--- SkinPlayer/SkinPlayerApp.swift
+++ SkinPlayer/SkinPlayerApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import AVFAudio
+import AVFoundation
 
 @main
 struct SkinPlayerApp: App {
+    
+    init() {
+        configureAudioSession()
+    }
+    
+    private func configureAudioSession() {
+        // Retrieve the shared audio session.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            // Set the audio session category and mode.
+            try audioSession.setCategory(.playback, mode: .moviePlayback)
+        } catch {
+            print("Failed to set the audio session configuration")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
