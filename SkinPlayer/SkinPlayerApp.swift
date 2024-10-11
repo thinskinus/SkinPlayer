b7953b2c28e695817361ed3ff7f4b2e067b5b315
Elena Demidova <thinskinus@gmail.com>
2024-10-11 13:44:39 +0300

    Initial Commit

---
+++ SkinPlayer/SkinPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinPlayerApp.swift
+//  SkinPlayer
+//
+//  Created by Elena Demidova on 11.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SkinPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
